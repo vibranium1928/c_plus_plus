ba2b906df273c2bd9c769119f3ac0c3eed4e15af
vibranium1928 <mohd.sabir.1928@gmail.com>
2024-12-17 20:09:42 +0530

    updated code

---
+++ multiply.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Congratulations. Welcome to multiply.cpp.";
+    return 0;
+}   
+
---
+++ new.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Congratulations. Welcome to new.cpp.";
+    return 0;
+}   
+
